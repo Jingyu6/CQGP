d322672c098e008b3c0dd6ad10981b6ea49e7c49
songyan-hou <815759459@qq.com>
2021-11-16 20:28:24 +0100

    add CQL soft

--- algos/cql_soft.py
+++ algos/cql_soft.py
@@ -17,102 +17,11 @@ from d3rlpy.gpu import Device
 from d3rlpy.models.encoders import EncoderFactory
 from d3rlpy.models.optimizers import AdamFactory, OptimizerFactory
 from d3rlpy.models.q_functions import QFunctionFactory
-from d3rlpy.algos.base import AlgoBase
-from d3rlpy.algos.dqn import DoubleDQN
+from d3rlpy.algos.cql import CQL, DiscreteCQL
 from .torch_impl.cql_soft_impl import CQLSoftImpl, DiscreteCQLSoftImpl
 
 
-class CQL_Soft(AlgoBase):
-    r"""Conservative Q-Learning algorithm.
-
-    CQL is a SAC-based data-driven deep reinforcement learning algorithm, which
-    achieves state-of-the-art performance in offline RL problems.
-
-    CQL mitigates overestimation error by minimizing action-values under the
-    current policy and maximizing values under data distribution for
-    underestimation issue.
-
-    .. math::
-
-        L(\theta_i) = \alpha\, \mathbb{E}_{s_t \sim D}
-            \left[\log{\sum_a \exp{Q_{\theta_i}(s_t, a)}}
-             - \mathbb{E}_{a \sim D} \big[Q_{\theta_i}(s, a)\big] - \tau\right]
-            + L_\mathrm{SAC}(\theta_i)
-
-    where :math:`\alpha` is an automatically adjustable value via Lagrangian
-    dual gradient descent and :math:`\tau` is a threshold value.
-    If the action-value difference is smaller than :math:`\tau`, the
-    :math:`\alpha` will become smaller.
-    Otherwise, the :math:`\alpha` will become larger to aggressively penalize
-    action-values.
-
-    In continuous control, :math:`\log{\sum_a \exp{Q(s, a)}}` is computed as
-    follows.
-
-    .. math::
-
-        \log{\sum_a \exp{Q(s, a)}} \approx \log{\left(
-            \frac{1}{2N} \sum_{a_i \sim \text{Unif}(a)}^N
-                \left[\frac{\exp{Q(s, a_i)}}{\text{Unif}(a)}\right]
-            + \frac{1}{2N} \sum_{a_i \sim \pi_\phi(a|s)}^N
-                \left[\frac{\exp{Q(s, a_i)}}{\pi_\phi(a_i|s)}\right]\right)}
-
-    where :math:`N` is the number of sampled actions.
-
-    The rest of optimization is exactly same as :class:`d3rlpy.algos.SAC`.
-
-    References:
-        * `Kumar et al., Conservative Q-Learning for Offline Reinforcement
-          Learning. <https://arxiv.org/abs/2006.04779>`_
-
-    Args:
-        actor_learning_rate (float): learning rate for policy function.
-        critic_learning_rate (float): learning rate for Q functions.
-        temp_learning_rate (float):
-            learning rate for temperature parameter of SAC.
-        alpha_learning_rate (float): learning rate for :math:`\alpha`.
-        actor_optim_factory (d3rlpy.models.optimizers.OptimizerFactory):
-            optimizer factory for the actor.
-        critic_optim_factory (d3rlpy.models.optimizers.OptimizerFactory):
-            optimizer factory for the critic.
-        temp_optim_factory (d3rlpy.models.optimizers.OptimizerFactory):
-            optimizer factory for the temperature.
-        alpha_optim_factory (d3rlpy.models.optimizers.OptimizerFactory):
-            optimizer factory for :math:`\alpha`.
-        actor_encoder_factory (d3rlpy.models.encoders.EncoderFactory or str):
-            encoder factory for the actor.
-        critic_encoder_factory (d3rlpy.models.encoders.EncoderFactory or str):
-            encoder factory for the critic.
-        q_func_factory (d3rlpy.models.q_functions.QFunctionFactory or str):
-            Q function factory.
-        batch_size (int): mini-batch size.
-        n_frames (int): the number of frames to stack for image observation.
-        n_steps (int): N-step TD calculation.
-        gamma (float): discount factor.
-        tau (float): target network synchronization coefficiency.
-        n_critics (int): the number of Q functions for ensemble.
-        target_reduction_type (str): ensemble reduction method at target value
-            estimation. The available options are
-            ``['min', 'max', 'mean', 'mix', 'none']``.
-        initial_temperature (float): initial temperature value.
-        initial_alpha (float): initial :math:`\alpha` value.
-        alpha_threshold (float): threshold value described as :math:`\tau`.
-        conservative_weight (float): constant weight to scale conservative loss.
-        n_action_samples (int): the number of sampled actions to compute
-            :math:`\log{\sum_a \exp{Q(s, a)}}`.
-        soft_q_backup (bool): flag to use SAC-style backup.
-        use_gpu (bool, int or d3rlpy.gpu.Device):
-            flag to use GPU, device ID or device.
-        scaler (d3rlpy.preprocessing.Scaler or str): preprocessor.
-            The available options are `['pixel', 'min_max', 'standard']`.
-        action_scaler (d3rlpy.preprocessing.ActionScaler or str):
-            action preprocessor. The available options are ``['min_max']``.
-        reward_scaler (d3rlpy.preprocessing.RewardScaler or str):
-            reward preprocessor. The available options are
-            ``['clip', 'min_max', 'standard']``.
-        impl (d3rlpy.algos.torch.cql_impl.CQLImpl): algorithm implementation.
-
-    """
+class CQL_Soft(CQL):
 
     _actor_learning_rate: float
     _critic_learning_rate: float
@@ -238,89 +147,9 @@ class CQL_Soft(AlgoBase):
         )
         self._impl.build()
 
-    def _update(self, batch: TransitionMiniBatch) -> Dict[str, float]:
-        assert self._impl is not None, IMPL_NOT_INITIALIZED_ERROR
-
-        metrics = {}
-
-        # lagrangian parameter update for SAC temperature
-        if self._temp_learning_rate > 0:
-            temp_loss, temp = self._impl.update_temp(batch)
-            metrics.update({"temp_loss": temp_loss, "temp": temp})
-
-        # lagrangian parameter update for conservative loss weight
-        if self._alpha_learning_rate > 0:
-            alpha_loss, alpha = self._impl.update_alpha(batch)
-            metrics.update({"alpha_loss": alpha_loss, "alpha": alpha})
-
-        critic_loss = self._impl.update_critic(batch)
-        metrics.update({"critic_loss": critic_loss})
-
-        actor_loss = self._impl.update_actor(batch)
-        metrics.update({"actor_loss": actor_loss})
-
-        self._impl.update_critic_target()
-        self._impl.update_actor_target()
-
-        return metrics
-
-    def get_action_type(self) -> ActionSpace:
-        return ActionSpace.CONTINUOUS
 
 
-class DiscreteCQL(DoubleDQN):
-    r"""Discrete version of Conservative Q-Learning algorithm.
-
-    Discrete version of CQL is a DoubleDQN-based data-driven deep reinforcement
-    learning algorithm (the original paper uses DQN), which achieves
-    state-of-the-art performance in offline RL problems.
-
-    CQL mitigates overestimation error by minimizing action-values under the
-    current policy and maximizing values under data distribution for
-    underestimation issue.
-
-    .. math::
-
-        L(\theta) = \alpha \mathbb{E}_{s_t \sim D}
-            [\log{\sum_a \exp{Q_{\theta}(s_t, a)}}
-             - \mathbb{E}_{a \sim D} [Q_{\theta}(s, a)]]
-            + L_{DoubleDQN}(\theta)
-
-    References:
-        * `Kumar et al., Conservative Q-Learning for Offline Reinforcement
-          Learning. <https://arxiv.org/abs/2006.04779>`_
-
-    Args:
-        learning_rate (float): learning rate.
-        optim_factory (d3rlpy.models.optimizers.OptimizerFactory):
-            optimizer factory.
-        encoder_factory (d3rlpy.models.encoders.EncoderFactory or str):
-            encoder factory.
-        q_func_factory (d3rlpy.models.q_functions.QFunctionFactory or str):
-            Q function factory.
-        batch_size (int): mini-batch size.
-        n_frames (int): the number of frames to stack for image observation.
-        n_steps (int): N-step TD calculation.
-        gamma (float): discount factor.
-        n_critics (int): the number of Q functions for ensemble.
-        target_reduction_type (str): ensemble reduction method at target value
-            estimation. The available options are
-            ``['min', 'max', 'mean', 'mix', 'none']``.
-        target_update_interval (int): interval to synchronize the target
-            network.
-        alpha (float): the :math:`\alpha` value above.
-        use_gpu (bool, int or d3rlpy.gpu.Device):
-            flag to use GPU, device ID or device.
-        scaler (d3rlpy.preprocessing.Scaler or str): preprocessor.
-            The available options are `['pixel', 'min_max', 'standard']`
-        reward_scaler (d3rlpy.preprocessing.RewardScaler or str):
-            reward preprocessor. The available options are
-            ``['clip', 'min_max', 'standard']``.
-        impl (d3rlpy.algos.torch.cql_impl.DiscreteCQLImpl):
-            algorithm implementation.
-
-    """
-
+class DiscreteCQL(DiscreteCQL):
     _alpha: float
     _impl: Optional[DiscreteCQLSoftImpl]
 
