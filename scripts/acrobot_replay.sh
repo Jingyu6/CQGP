a320917393009cb7b3911e6a1293c179c6885fb7
Jingyu6 <liujingyu@student.ethz.ch>
2022-01-13 10:03:18 +0100

    Added new environments and plots

---
+++ scripts/acrobot_replay.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+python run.py -e acrobot-replay -n 5 -x 20 -i 25
---
+++ scripts/gen_acrobot_replay.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+python datasets.py --env_name=Acrobot-v1 --total_timesteps=50000 --dataset_name=acrobot_replay
